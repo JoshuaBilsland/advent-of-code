c53fc416a1d1d4dacf736017304166ceece23a3a
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-12-02 19:54:53 +0000

    feat: solve d2p1

--- 2024/day-02/main.py
+++ 2024/day-02/main.py
@@ -7,7 +7,19 @@ def extract_reports(file_path):
 
 
 def main():
+    # ***Puzzle 1 Solution***
     reports_list = extract_reports("2024/day-02/input.txt")
+    safe_reports_num = 0
+    for report in reports_list:
+        report = list(map(int, report))
+        # zip() is used to pair the each element with the one after it
+        # each pair is then checked to see if it is asc/desc and if they differ by 1-3
+        # all checks that these conditions are true for each pair
+        if all((currentIndex < nextIndex) and (abs(currentIndex - nextIndex) >= 1 and abs(currentIndex - nextIndex) <= 3) for currentIndex, nextIndex in zip(report, report[1:])):
+            safe_reports_num += 1
+        elif all((currentIndex > nextIndex) and (abs(currentIndex - nextIndex) >= 1 and abs(currentIndex - nextIndex) <= 3) for currentIndex, nextIndex in zip(report, report[1:])):
+            safe_reports_num += 1
+    print(safe_reports_num)
 
 
 if __name__ == "__main__":
