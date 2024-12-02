38c51f01436fb95c40f7922f2372fa54760bd2bd
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-12-02 21:16:00 +0000

    feat: solve d2p2

--- 2024/day-02/main.py
+++ 2024/day-02/main.py
@@ -6,6 +6,19 @@ def extract_reports(file_path):
     return reports
 
 
+def is_safe(report):
+    """Check if a given report is safe
+
+    Args:
+        report (list): A list of levels (the report to check)
+    """
+    return (
+        all((currentIndex < nextIndex) and (abs(currentIndex - nextIndex) >= 1 and abs(currentIndex - nextIndex) <= 3) for currentIndex, nextIndex in zip(report, report[1:]))
+        or
+        all((currentIndex > nextIndex) and (abs(currentIndex - nextIndex) >= 1 and abs(currentIndex - nextIndex) <= 3) for currentIndex, nextIndex in zip(report, report[1:]))
+    )
+
+
 def main():
     # ***Puzzle 1 Solution***
     reports_list = extract_reports("2024/day-02/input.txt")
@@ -19,6 +32,21 @@ def main():
             safe_reports_num += 1
         elif all((currentIndex > nextIndex) and (abs(currentIndex - nextIndex) >= 1 and abs(currentIndex - nextIndex) <= 3) for currentIndex, nextIndex in zip(report, report[1:])):
             safe_reports_num += 1
+    print(safe_reports_num)
+
+    # ***Puzzle 2 Solution***
+    safe_reports_num = 0
+    for report in reports_list:
+        report = list(map(int, report))
+        if is_safe(report):
+            safe_reports_num += 1
+        else:
+            # Check if the report is valid when a level is removed
+            for level_index in range(len(report)):
+                modified_report = report[:level_index] + report[level_index+1:]
+                if is_safe(modified_report):
+                    safe_reports_num += 1
+                    break
     print(safe_reports_num)
 
 
