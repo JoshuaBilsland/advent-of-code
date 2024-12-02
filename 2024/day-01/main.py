110eb22c146aa355b7084913deda9c6aa03b2ef3
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-12-02 11:40:44 +0000

    feat: solve d1p1

---
+++ 2024/day-01/main.py
@@ -0,0 +1,29 @@
+import merge_sort as ms
+
+
+# sorts the lists into ascending order
+def get_lists(file_path):
+    list_one = []
+    list_two = []
+    with open(file_path, "r") as file:
+        for line in file.readlines():
+            list_one.append(line[:line.index("   ")])
+            list_two.append(line[line.index("   ")+3:-1])
+    return list_one, list_two
+
+
+def main():
+    # ***Puzzle 1 Solution***
+    list_one, list_two = get_lists("2024/day-01/input.txt")
+    list_one = ms.merge_sort(list_one)
+    list_two = ms.merge_sort(list_two)
+
+    total = 0
+    # go through each pair and return the difference without any sign
+    for num_one, num_two in zip(list_one, list_two):
+        total += abs(int(num_one) - int(num_two))
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
