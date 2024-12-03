b3d2e7b72d5407ae940500fe394cfca84c6e8095
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-12-03 18:44:01 +0000

    feat: solve d3p2

--- 2024/day-03/main.py
+++ 2024/day-03/main.py
@@ -23,6 +23,26 @@ def main():
             total += nums[0] * nums[1]
     print(total)
 
+    # ***Puzzle 2 Solution***
+    memory_lines = extract_memory("2024/day-03/input.txt")
+    pattern = r"do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)"
+    total = 0
+    instructions_enable = True
+    for line in memory_lines:
+        # create list of mul(x,y) from the "memory"
+        matches = re.findall(pattern, line)
+        # Go through each match, checking for do, don't and mul()
+        for match in matches:
+            if match == "do()":
+                instructions_enable = True
+            elif match == "don't()":
+                instructions_enable = False
+            else:
+                if instructions_enable:
+                    nums = list(map(int, re.findall(r"\d+", match)))
+                    total += nums[0] * nums[1]
+    print(total)
+
 
 if __name__ == "__main__":
     main()
