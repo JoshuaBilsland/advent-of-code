5246912fde36632a21c74c2a234310a3c0344db7
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-12-05 22:03:01 +0000

    feat: solve d4p2

--- 2024/day-04/main.py
+++ 2024/day-04/main.py
@@ -47,10 +47,58 @@ def check_around(grid):
     return target_found_count
 
 
+def part_two_check_around(grid):
+    """Check each direction for "MAS"s in the shape of an "X"."""
+    mas_cross_count = 0
+
+    directions = [
+        [-1, -1],  # Up-left
+        [1, 1],     # Down-right
+
+        [-1, 1],   # Up-right
+        [1, -1],   # Down-left
+    ]
+
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column] == "A":
+                is_match = True
+
+                up_left = list((row + directions[0][0], column + directions[0][1]))
+                down_right = list((row + directions[1][0], column + directions[1][1]))
+
+                up_right = list((row + directions[2][0], column + directions[2][1]))
+                down_left = list((row + directions[3][0], column + directions[3][1]))
+
+                # Check one diagonal
+                if (all(0 <= x < len(grid) for x in up_left) and all(0 <= x < len(grid) for x in down_right)):
+                    if not (grid[up_left[0]][up_left[1]] == "M" and grid[down_right[0]][down_right[1]] == "S" or
+                            grid[up_left[0]][up_left[1]] == "S" and grid[down_right[0]][down_right[1]] == "M"):
+                        is_match = False
+                else:  # Change from directions would mean index outside of the grid
+                    is_match = False
+
+                # Check the other diagonal
+                if (all(0 <= x < len(grid) for x in up_right) and all(0 <= x < len(grid) for x in down_left)):
+                    if not (grid[up_right[0]][up_right[1]] == "M" and grid[down_left[0]][down_left[1]] == "S" or
+                            grid[up_right[0]][up_right[1]] == "S" and grid[down_left[0]][down_left[1]] == "M"):
+                        is_match = False
+                else:  # Change from directions would mean index outside of the grid
+                    is_match = False
+
+                if is_match:
+                    mas_cross_count += 1
+
+    return mas_cross_count
+
+
 def main():
     # ***Problem 1 Solution***
     word_search = extract_word_search("2024/day-04/input.txt")
     print(check_around(word_search))
+
+    # ***Problem 2 Solution***
+    print(part_two_check_around(word_search))
 
 
 if __name__ == "__main__":
